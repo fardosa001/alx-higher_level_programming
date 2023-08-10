19f986faca70060f8e7d382ce1bd1e976955f7e9
fardosa001 <fardosaadow@gmail.com>
2023-08-10 02:24:15 -0700

    Remove at position

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if n < 0 or n >= len(str):
+        return str
+
+    result = ""
+    for j in range(len(str)):
+        if j != n:
+            result += str[j]
+    return result
