4ba8488882422826d711948733b0cfea54a22acc
fardosa001 <fardosaadow@gmail.com>
2023-08-18 08:29:39 -0700

    Roman to Integer

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if type(roman_string) is None or type(roman_string) is not str:
+        return 0
+
+    roman_numerals = {
+            'I': 1, 'V': 5, 'X': 10,
+            'L': 50, 'M': 1000, 'D': 500, 'C': 100
+            }
+
+    result = 0
+    prev_num = 0
+
+    for char in reversed(roman_string):
+        num = roman_numerals[char]
+        if num >= prev_num:
+            result += num
+
+        else:
+            result -= num
+            prev_num = num
+
+    return result
