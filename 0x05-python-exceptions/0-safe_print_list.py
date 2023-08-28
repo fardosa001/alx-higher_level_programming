cc946d31d1dea44c060686d69d4d380fa8ca4ab1
fardosa001 <fardosaadow@gmail.com>
2023-08-28 05:37:43 -0700

    Safe list printing

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        num = 0
+        for n in my_list:
+            if x > num:
+                print(n, end="")
+                num += 1
+        print()
+        return num
+    except:
+        return num
