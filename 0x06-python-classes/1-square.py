1ecede9d269d84afadd315191e8add482cf19de6
fardosa001 <fardosaadow@gmail.com>
2023-08-29 08:04:29 -0700

    Square with size

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -6,16 +6,11 @@ Defines a square with a private instance attribute size
 
 class Square:
 
-    def __init__(self, size=0):
+    def __init__(self, size):
         """__init__
         __init__ method initializes the size value of a Square.
 
         Attributes:
             __size(int): size of the square
         """
-        if type(size) is not int:
-            raise TypeError("size must be integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.__size = size
