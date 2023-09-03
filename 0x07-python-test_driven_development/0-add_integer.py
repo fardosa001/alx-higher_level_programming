16f7797f041de3a3214bb4cf867f08b99fef61dc
fardosa001 <fardosaadow@gmail.com>
2023-09-03 06:32:01 -0700

    Integers addition mandatory

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    """Addition function
+    Attributes:
+        a:first parameter could be an int or a float
+        b:second parameter could be an int or a float
+    Returns: sum of a and b
+    Exceptions: 
+        raises TypeError("a must be an integer or b must be an integer")
+
+    """
+    if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+        raise TypeError("a must be an integer or b must be an integer")
+    
+    a = int(a) if isinstance(a, float) else a
+    b = int(b) if isinstance(b, float) else b
+
+    return int(a + b)
+    
