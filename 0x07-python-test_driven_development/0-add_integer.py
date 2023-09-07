58d31b3a73f0b9a3a504b6d026131b801397b00f
fardosa001 <fardosaadow@gmail.com>
2023-09-07 06:56:23 -0700

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -21,3 +21,18 @@ def add_integer(a, b=98):
     b = int(b) if isinstance(b, float) else b
 
     return int(a + b)
+
+
+if __name__ == "__main__":
+    print(add_integer(1, 2))
+print(add_integer(100, -2))
+print(add_integer(2))
+print(add_integer(100.3, -2))
+try:
+    print(add_integer(4, "School"))
+except Exception as e:
+    print(e)
+try:
+    print(add_integer(None))
+except Exception as e:
+    print(e)
