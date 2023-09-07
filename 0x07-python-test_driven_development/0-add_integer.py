514816438793eb772d9c00e9476a774aed081df0
fardosa001 <fardosaadow@gmail.com>
2023-09-07 06:34:23 -0700

    fix

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
+"""module 0-add_integer"""
+
+
 def add_integer(a, b=98):
     """Addition function
     Attributes:
         a:first parameter could be an int or a float
         b:second parameter could be an int or a float
-    Returns: sum of a and b
-    Exceptions: 
+    Return: sum of a and b
+    Exceptions:
         raises TypeError("a must be an integer or b must be an integer")
 
     """
@@ -13,9 +16,8 @@ def add_integer(a, b=98):
         raise TypeError('a must be an integer')
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
+
     a = int(a) if isinstance(a, float) else a
     b = int(b) if isinstance(b, float) else b
 
     return int(a + b)
-    
