9ff908204e132d553edbf80aa457bd230488cddf
fardosa001 <fardosaadow@gmail.com>
2023-09-11 11:41:12 -0700

    Only sub class of

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,6 @@
 """returns True if the object is an instance of a class that inherited from"""
 
 
-
 def inherits_from(obj, a_class):
     """returns True if the object is an instance of a class that inherited
         (directly or indirectly) from the specified class ; otherwise False.
