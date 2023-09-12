54233cd7a03dbaa4723f37106ab6f71e09926957
fardosa001 <fardosaadow@gmail.com>
2023-09-12 11:54:08 -0700

    Load, add, save

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
+"""script that adds all arguments to a Python list,
+and then save them to a file"""
+
+
 from sys import argv
 from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-"""script that adds all arguments to a Python list,
-and then save them to a file"""
-
 
 if path.exists('add_item.json'):
 
