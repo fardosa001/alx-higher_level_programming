aca7336d18ff25021288d7009fcbcf9ce0b28aa2
fardosa001 <fardosaadow@gmail.com>
2023-09-12 06:36:42 -0700

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""returns an object (Python data structure) represented by a JSON string""""
+"""returns an object (Python data structure) represented by a JSON string"""
 from json import loads
 
 
