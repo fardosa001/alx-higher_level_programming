09d3c17ce9838cc0aca45cb97cb9714e5c0807aa
fardosa001 <fardosaadow@gmail.com>
2023-10-18 11:30:01 -0700

    Not my genre

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,11 +1,13 @@
 -- script that uses the hbtn_0d_tvshows database to list
 -- all genres not linked to the show Dexter
-
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS 'rating'
-	FROM tv_genres
+SELECT tv_genres.name
+      FROM tv_genres
+     WHERE tv_genres.name NOT IN (
+    SELECT tv_genres.name
+      FROM tv_genres
 INNER JOIN tv_show_genres
-	ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_show_ratings
-	ON tv_show_ratings.show_id = tv_show_genres.show_id
-GROUP BY name
-ORDER BY rating DESC;
+        ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+	ON tv_show_genres.show_id = tv_shows.id
+     WHERE tv_shows.title = "Dexter")
+  ORDER BY tv_genres.name ASC;
