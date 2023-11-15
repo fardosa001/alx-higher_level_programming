92473dc264012d0abae7ec49edee248f1ada6483
root <fardosaadow@gmail.com>
2023-11-15 11:36:36 -0800

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -4,7 +4,7 @@ const num = parseInt(process.argv[2]);
 
 if (num) {
   for (let j = 0; j < num; j++) {
-   console.log('C is fun');
+    console.log('C is fun');
   }
 } else {
   console.log('Missing number of occurrences');
