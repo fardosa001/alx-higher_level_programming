bfc57cdef03377117d31a9d893176c3aceb0c913
root <fardosaadow@gmail.com>
2023-11-15 04:44:19 -0800

    Rectangle #2

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,16 @@
 #!/usr/bin/node
 
-
 module.exports = class Rectangle {
-	constructor(w, h) {
-		if (w < 0 || h > 0) {
-			this.width = w;
-			this.height = h;
+  constructor (w, h) {
+    if (w < 0 || h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
 
-		}
-	}
-	print() {
-	for (let r = 0; r < this.height; r++) {
-		console.log("X".repeat(this.width));
-		}
-	}
-
+  print () {
+    for (let r = 0; r < this.height; r++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 };
