e1699acdb7a114f987675ae1b689b4a1bb32e770
root <fardosaadow@gmail.com>
2023-11-15 07:49:53 -0800

    Rectangle #4

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -2,7 +2,7 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w < 0 || h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
@@ -15,13 +15,13 @@ module.exports = class Rectangle {
   }
 
   rotate () {
-    const temp = this.height;
-    this.height = this.width;
-    this.width = temp;
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
   }
 
   double () {
-    this.height *= 2;
     this.width *= 2;
+    this.height *= 2;
   }
 };
