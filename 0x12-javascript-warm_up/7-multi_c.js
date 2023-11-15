edb4028c09899fe08d17092864b011ba1dbf0a15
root <fardosaadow@gmail.com>
2023-11-15 11:34:25 -0800

    I love C

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+if (num) {
+  for (let j = 0; j < num; j++) {
+   console.log('C is fun');
+  }
+} else {
+  console.log('Missing number of occurrences');
+}
