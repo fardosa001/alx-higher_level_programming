466e28dbfe18808200df9f90dd605b591e5908e2
fardosa001 <fardosaadow@gmail.com>
2024-01-03 04:22:09 -0800

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,10 +1,17 @@
 #!/usr/bin/node
 
-const id = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/$id';
 
 const request = require('request');
-request(url, function (_err, _res, body) {
-  body = JSON.parse(body);
-  console.log(body.tittle);
+let id = process.argv[2];
+let url = 'https://swapi-api.alx-tools.com/api/films/' + id;
+
+request(url, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else if (res.statusCode === 200) {
+    body = JSON.parse(body);
+    console.log(body.title);
+  } else {
+    console.log('Error code:' + res.statusCode);
+  }
 });
