fc6a045610720e86114b2efd1c7577ad30d79f72
fardosa001 <fardosaadow@gmail.com>
2024-01-10 20:06:05 +0300

    No jQuery - document loaded

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const headerElement = document.querySelector('header');
+
+    // Update the text color to red (#FF0000)
+    if (headerElement) {
+      headerElement.style.color = '#FF0000';
+    }
+});
